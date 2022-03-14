6be136e37bcbc0ab0ebbd96648911d6d873fcaa0
sam <syed.simanta06@gmail.com>
2022-03-14 16:07:57 +0600

    added

--- src/contracts/Tether.sol
+++ src/contracts/Tether.sol
@@ -1,8 +1,12 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.0;
 
 contract Tether {
-  string public name = 'Tether';
-  string public symbol = 'USDT';
-  uint256 public totalSupply = 10000000000000000000;
-  uint8 public decimal =18;
+    string public name = "Tether";
+    string public symbol = "USDT";
+    uint256 public totalSupply = 10000000000000000000;
+    uint8 public decimal = 18;
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approve(address indexed _owner, address indexed _to, uint256 _value);
 }
